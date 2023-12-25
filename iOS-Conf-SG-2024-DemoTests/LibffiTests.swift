394c28eee6bf8791f61d3cd63ae795196dc69065
ya.wang <ya.wang@okg.com>
2023-12-25 23:01:12 +0800

    Separate test cases between Libffi and SwiftHook

--- iOS-Conf-SG-2024-DemoTests/LibffiTests.swift
+++ iOS-Conf-SG-2024-DemoTests/LibffiTests.swift
@@ -1,62 +1,14 @@
 //
-//  iOS_Conf_SG_2024_DemoTests.swift
+//  LibffiTests.swift
 //  iOS-Conf-SG-2024-DemoTests
 //
-//  Created by Wang Ya on 22/12/23.
+//  Created by Wang Ya on 25/12/23.
 //
 
 import XCTest
-import EasySwiftHook
 import libffi_iOS
-@testable import iOS_Conf_SG_2024_Demo
 
-final class iOS_Conf_SG_2024_DemoTests: XCTestCase {
-    
-    func testHookBeforeAfter() throws {
-        
-        class Developer {
-            @objc dynamic func coding(featureName: String) { // The key words of methods `@objc` and `dynamic` are necessary.
-                print("codding for feature \"\(featureName)\"")
-            }
-        }
-        
-        let dev = Developer()
-        
-        try hookBefore(object: dev, selector: #selector(Developer.coding)) {
-            print("open Xcode.")
-        }
-        try hookAfter(object: dev, selector: #selector(Developer.coding), closure: { obj, sel, featureName in
-            print("git commit -m \"\(featureName)\"")
-            print("close Xcode.")
-        } as @convention(block) (AnyObject, Selector, String) -> Void)
-        
-        dev.coding(featureName: "Use of ImpressionKit")
-        
-    }
-    
-    func testHookInstead() throws {
-        class Computer {
-            @objc dynamic func sum(leftNumber: Int, rightNumber: Int) -> Int {
-                print("calculating")
-                return leftNumber + rightNumber
-            }
-        }
-        
-        let computer = Computer()
-        
-        try hookInstead(object: computer, selector: #selector(Computer.sum(leftNumber:rightNumber:)), closure: { original, obj, sel, leftNumber, rightNumber in
-            print("before calculating, leftNumber = \(leftNumber), rightNumber = \(rightNumber)")
-            let originalResult = original(obj, sel, leftNumber, rightNumber)
-            print("after calculating, got result = \(originalResult)")
-            print("hook and return 99")
-            return 99
-        } as @convention(block) ((AnyObject, Selector, Int, Int) -> Int, AnyObject, Selector, Int, Int) -> Int)
-        
-        let leftNumber = 3
-        let rightNumber = 4
-        let result = computer.sum(leftNumber: leftNumber, rightNumber: rightNumber)
-        print("\(leftNumber) + \(rightNumber) = \(result)")
-    }
+final class LibffiTests: XCTestCase {
     
     static let targetFuntion = {
         print("Target function is called")
@@ -75,12 +27,12 @@ final class iOS_Conf_SG_2024_DemoTests: XCTestCase {
                 assertionFailure()
                 return
             }
-            ffi_call(cifPointer, iOS_Conf_SG_2024_DemoTests.targetFuntion, nil, nil)
+            ffi_call(cifPointer, LibffiTests.targetFuntion, nil, nil)
         }
     }
     
     static let targetFunctionBinding = { cif, ret, arges, userdata in
-        ffi_call(cif, iOS_Conf_SG_2024_DemoTests.targetFuntion, ret, arges)
+        ffi_call(cif, LibffiTests.targetFuntion, ret, arges)
     } as @convention(c) (_ cif: UnsafeMutablePointer<ffi_cif>?, _ ret: UnsafeMutableRawPointer?, _ args: UnsafeMutablePointer<UnsafeMutableRawPointer?>?, _ userdata: UnsafeMutableRawPointer?) -> Void
    
     func testFFIClosure() {
@@ -108,7 +60,7 @@ final class iOS_Conf_SG_2024_DemoTests: XCTestCase {
             guard (ffi_prep_closure_loc(
                 closurePointer,
                 cifPointer,
-                iOS_Conf_SG_2024_DemoTests.targetFunctionBinding,
+                LibffiTests.targetFunctionBinding,
                 nil,
                 boundTargetFunctionPointer)) == FFI_OK else {
                 assertionFailure()
@@ -120,4 +72,5 @@ final class iOS_Conf_SG_2024_DemoTests: XCTestCase {
             ffi_closure_free(closurePointer);
         }
     }
+    
 }
---
+++ iOS-Conf-SG-2024-DemoTests/SwiftHookTests.swift
@@ -0,0 +1,59 @@
+//
+//  SwiftHookTests.swift
+//  iOS-Conf-SG-2024-DemoTests
+//
+//  Created by Wang Ya on 25/12/23.
+//
+
+import XCTest
+import EasySwiftHook
+
+final class SwiftHookTests: XCTestCase {
+    
+    func testHookBeforeAfter() throws {
+        
+        class Developer {
+            @objc dynamic func coding(featureName: String) { // The key words of methods `@objc` and `dynamic` are necessary.
+                print("codding for feature \"\(featureName)\"")
+            }
+        }
+        
+        let dev = Developer()
+        
+        try hookBefore(object: dev, selector: #selector(Developer.coding)) {
+            print("open Xcode.")
+        }
+        try hookAfter(object: dev, selector: #selector(Developer.coding), closure: { obj, sel, featureName in
+            print("git commit -m \"\(featureName)\"")
+            print("close Xcode.")
+        } as @convention(block) (AnyObject, Selector, String) -> Void)
+        
+        dev.coding(featureName: "Use of ImpressionKit")
+        
+    }
+    
+    func testHookInstead() throws {
+        class Computer {
+            @objc dynamic func sum(leftNumber: Int, rightNumber: Int) -> Int {
+                print("calculating")
+                return leftNumber + rightNumber
+            }
+        }
+        
+        let computer = Computer()
+        
+        try hookInstead(object: computer, selector: #selector(Computer.sum(leftNumber:rightNumber:)), closure: { original, obj, sel, leftNumber, rightNumber in
+            print("before calculating, leftNumber = \(leftNumber), rightNumber = \(rightNumber)")
+            let originalResult = original(obj, sel, leftNumber, rightNumber)
+            print("after calculating, got result = \(originalResult)")
+            print("hook and return 99")
+            return 99
+        } as @convention(block) ((AnyObject, Selector, Int, Int) -> Int, AnyObject, Selector, Int, Int) -> Int)
+        
+        let leftNumber = 3
+        let rightNumber = 4
+        let result = computer.sum(leftNumber: leftNumber, rightNumber: rightNumber)
+        print("\(leftNumber) + \(rightNumber) = \(result)")
+    }
+    
+}
